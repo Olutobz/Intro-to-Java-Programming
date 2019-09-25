ef46a4d07800f8fdd4bc5091040373ab8d7002ff
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2019-09-25 18:42:16 +0100

    Contains chapter 4 solution 1-10

---
+++ solutions/chapter4/Exercise_04_07.java
@@ -0,0 +1,11 @@
+package chapter4;
+/* (Corner point coordinates) Suppose a pentagon is centered at (0, 0) with one point
+at the 0 o’clock position, as shown in Figure 4.7c. Write a program that prompts
+the user to enter the radius of the bounding circle of a pentagon and displays the
+coordinates of the five corner points on the pentagon. */
+
+public class Exercise_04_07 {
+    public static void main(String[] args) {
+        //Todo: supply answers later
+    }
+}
