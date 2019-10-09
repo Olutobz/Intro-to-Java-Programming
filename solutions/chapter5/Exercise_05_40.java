f7ea3d94b9d64d7bf7223007602eb07e22421c54
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2019-10-09 20:49:16 +0100

    Added solution exercise for chp5 to 40

---
+++ solutions/chapter5/Exercise_05_40.java
@@ -0,0 +1,25 @@
+package chapter5;
+/*
+(Simulation: heads or tails) Write a program that simulates flipping a coin one
+million times and displays the number of heads and tails.
+*/
+
+public class Exercise_05_40 {
+    public static void main(String[] args) {
+        int heads,                // Counts the number of heads
+                tails,                // Counts the number of tails
+                coin;                // Coin
+        heads = tails = 0;    // Initialize heads and tails to 0;
+
+        // Simulate flipping a coin one million times
+        System.out.println("Flipping a coin one million times..");
+        for (int i = 0; i < 1000000; i++) {
+            // Generate randomly 0 or 1.
+            // If 0 increment heads otherwise increment tails
+            coin = (int) (Math.random() * 2) == 0 ? heads++ : tails++;
+        }
+
+        // Display results
+        System.out.println("Heads: " + heads + "\nTails: " + tails);
+    }
+}
