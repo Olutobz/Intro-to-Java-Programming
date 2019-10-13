cd4eb68b58c74a9ef93657bc45da7bcee70c21fc
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2019-10-13 22:00:41 +0100

    Added solution 46 and 47 in chp5

---
+++ solutions/chapter5/Exercise_05_46.java
@@ -0,0 +1,26 @@
+package chapter5;
+
+import java.util.Scanner;
+/*
+(Reverse a string) Write a program that prompts the user to enter a string and
+displays the string in reverse order.
+*/
+
+public class Exercise_05_46 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a string
+        System.out.print("Enter a string: ");
+        String string = input.nextLine();
+
+        // Reverse the string
+        String reverse = "";
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reverse += string.charAt(i);
+        }
+
+        // Display the string in reverse order
+        System.out.println("The reversed string is " + reverse);
+    }
+}
