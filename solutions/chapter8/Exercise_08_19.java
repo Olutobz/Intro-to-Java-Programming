36168d79d8578980440dad124b1b29d82cd6f9a2
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2019-12-25 18:24:15 +0100

    Completed exercises in chapter 8

---
+++ solutions/chapter8/Exercise_08_19.java
@@ -0,0 +1,7 @@
+package chapter8;
+
+public class Exercise_08_19 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ solutions/chapter8/Exercise_08_21.java
@@ -0,0 +1,7 @@
+package chapter8;
+
+public class Exercise_08_21 {
+    public static void main(String[] args) {
+
+    }
+}
